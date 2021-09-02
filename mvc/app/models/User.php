e54f86e042f9ab390293ad0d7ac6ba62352205a1
jobokcoder <yonghoon_99@icloud.com>
2021-09-02 16:29:11 +0900

    Update MVC_Tutorial Database Insert Get Exception

--- mvc/app/models/User.php
+++ mvc/app/models/User.php
@@ -6,23 +6,25 @@ class User
     
     public function connect($name)
     {
-        
         $pg_db_host = 'localhost';
-        $pg_db_username = 'postgres'; // 자신이 설정한 DB 유저(role)이름. postgres는 기본 이름
-        $pg_db_name = 'postgres'; // 자신이 만든 DB 이름, postgres는 기본 DB이름
-        $pg_db_password = 'sample241'; // 자신이 설정한 비밀번호 입력
-
-        $dsn = "pgsql:host=$pg_db_host;dbname=$pg_db_name;port=5555";
-        // cf. 만약, mysql로 DB를 바꾸려면 pgsql을 mysql로 바꿔주기만 하면 됨. 이게 PDO의 장점
-
-        $pdo = new PDO($dsn, $pg_db_username, $pg_db_password); // PDO 객체 생성.
-
-        $stmt = $pdo->prepare("INSERT INTO test (id, name) VALUES (4, ?)");
-        $stmt->execute([$name]);
-
-        $pdo=null;
+        $pg_db_username = 'postgres';
+        $pg_db_name = 'postgres';
+        $pg_db_password = 'sample241';
+        
+        try {
+            $dsn = "pgsql:host=$pg_db_host;dbname=$pg_db_name;port=5555";
+            $pdo = new PDO($dsn, $pg_db_username, $pg_db_password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $pdo->prepare("INSERT INTO test (id, name) VALUES (7, ?)");
+            $success = $stmt->execute([$name]);
+            echo "success!";
+        } catch (PDOException $pe) {
+            die('PDOException : '. $pe->getMessage());
+        } catch (Exception $e) {
+            echo "Exception : $e";
+        }
+        
     }
 }
-
 
 ?>
